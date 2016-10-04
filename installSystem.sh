c022079974d7c10bb877cf6547a64160558e70f4
JStay <stay.jb@gmail.com>
2016-10-04 20:21:36 +0000

    Fixing install script

--- installSystem.sh
+++ installSystem.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Script to install the full Agora system.
 
-# Install dependencies
-apt-get install -y make libcairo2-dev libpng12-dev libjpeg-dev libossp-uuid-dev  freerdp-x11 libssh2-1 libfreerdp-dev libvorbis-dev gcc libssh-dev libpulse-dev tomcat7 tomcat7-admin tomcat7-docs libpango1.0-dev libssh2-1-dev git python-tk
-apt-get install -y libvncserver-dev
-apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps x11vnc
-apt-get install -y maven
-apt-get install -y default-jdk
+logFile=install_log.txt
 
+echo -e "Installing dependencies"
+# Install dependencies
+apt-get install -y make libcairo2-dev libpng12-dev libjpeg-dev libossp-uuid-dev  freerdp-x11 libssh2-1 libfreerdp-dev libvorbis-dev gcc libssh-dev libpulse-dev tomcat7 tomcat7-admin tomcat7-docs libpango1.0-dev libssh2-1-dev git python-tk >> $logFile
+apt-get install -y libvncserver-dev >> $logFile
+apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps x11vnc >> $logFile
+apt-get install -y maven >> $logFile
+apt-get install -y default-jdk >> $logFile
+
+echo -e "Finished\n\n\n"
+
+echo -e "\n\n\nDownload and install guacamole"
 # Download and install guacamole.
 wget -O guacamole-server-0.9.9.tar.gz http://sourceforge.net/projects/guacamole/files/current/source/guacamole-server-0.9.9.tar.gz/download
 tar -xvzf guacamole-server-0.9.9.tar.gz
@@ -18,10 +24,16 @@ update-rc.d guacd defaults
 ldconfig
 mkdir /etc/guacamole
 
+echo -e "Finished\n\n\n"
+
+echo -e "\n\n\nGet Agora from github"
 # Clone Agora repo to /home folder.
 cd /home
 git clone https://github.com/solsaver/Agora
 
+echo -e "Finished\n\n\n"
+
+echo -e "\n\n\nInstall guacamole no-auth extension"
 # Install guacamole-auth-noauth extension.
 wget -O guacamole-auth-noauth-0.9.8.tar.gz https://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-noauth-0.9.8.tar.gz/download
 tar -xvzf guacamole-auth-noauth-0.9.8.tar.gz
@@ -29,6 +41,8 @@ mkdir /etc/guacamole/extensions
 cp guacamole-auth-noauth-0.9.8/guacamole-auth-noauth-0.9.8.jar /etc/guacamole/extensions
 rm guacamole-auth-noauth-0.9.8.tar.gz
 rm -r guacamole-auth-noauth-0.9.8
+
+echo -e "Finished\n\n\n"
 
 # Copy over guacamole setting files.
 cp Agora/settings-guac/* /etc/guacamole
@@ -40,7 +54,8 @@ mkdir /usr/share/tomcat7/.guacamole
 ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7/.guacamole
 
 # make a link to the webapps files to override tomcat's default page with the Agora page.
-ln -s /home/Agora/webapps /var/lib/tomcat7/webapps
+rm -r /var/lib/tomcat7/webapps
+ln -s /home/Agora/webapps /var/lib/tomcat7/
 
 # Build the maven project then copy the resulting war file to the webapps directory.
 cd /home/Agora/guacamole-client/guacamole
