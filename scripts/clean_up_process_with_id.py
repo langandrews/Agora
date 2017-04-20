e44c1301cd0dd947d3022576e7186a7a7a501944
root <root@16fa-cs394-agora.cs.calvin.edu>
2017-04-20 14:40:00 +0000

    Fixed kill script, tab unload, and other less important stuff

--- scripts/clean_up_process_with_id.py
+++ scripts/clean_up_process_with_id.py
@@ -1,37 +1,16 @@
 import sys
+import xml.etree.ElementTree as ET
 
 # This program should be run like:
 # python2 clean_up_process_with_id.py $id
 args = sys.argv
 id = '-' + args[1] + '-'
 
-# Get all connections from noauth-config.xml
-file = open('/etc/guacamole/noauth-config.xml', 'r')
-lines = file.read().split('\n')
-port = ""
-for i in range(len(lines)):
-  if id in lines[i]:
-    # Get the port that we had used
-    port = str(int(lines[i+2].split("'")[3]) - 5900)
-    # We don't want to keep the lines that relate to this process
-    del lines[i:i+4]
-    break
+root = ET.parse('/etc/guacamole/noauth-config.xml').getroot()
 
-# Clean up the noauth-config
-file = open('/etc/guacamole/noauth-config.xml', 'w')
-del lines[-1]
-for line in lines:
-  file.write(line + "\n");
-file.close()
-
-# Remove this port from current_ports.txt
-print port
-file = open('/home/Agora/resources/current_ports.txt', 'r')
-lines = file.readlines()
-file.close()
-file = open('/home/Agora/resources/current_ports.txt', 'w')
-for line in lines:
-  if line != port+"\n":
-    print line
-    file.write(line)
-file.close()
+for child in root:
+    if id in child.attrib['name']:
+        root.remove(child)
+        break
+tree = ET.ElementTree(root)
+tree.write('/etc/guacamole/noauth-config.xml')
